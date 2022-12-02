c8406ef5d779cd2d3282c9716dad4bb2a39f23e0
fadastro <fadastro@gmail.com>
2022-12-02 11:56:32 +0200

    Correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
+for i in range(97, 123):
+    if chr(i) == 'q' or chr(i) == 'e':
         continue
-    else:
-        print("{:s}".format(chr(i)), end="")
+    print("{}".format(chr(i)), end="")
